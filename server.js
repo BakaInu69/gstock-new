1c0954a45484a67781be6e08a8955e0d58a2a3ab
davidqqq <david_tseng1992@hotmail.com>
2017-08-25 12:26:09 +0800

    test

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.get('/hello', function (req, res) {
   res.send('Hello World!')
 })
 app.get('/*', (req, res) => {
-  res.sendStatus(200).send(__dirname + 'dist/index.html')
-  res.sendFile(path.join(__dirname, 'dist/index.html'))
+  res.send('Hello World!')
+  //   res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 const port = process.env.port || 3000
 app.set('port', port)
