578d685a26f7cd00bbdc51fcac7bc6a59e6993f1
davidqqq <david_tseng1992@hotmail.com>
2017-08-25 12:09:44 +0800

    Check if server has dist folder

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.get('/*', (req, res) => {
+  res.sendStatus(200).send(__dirname + 'dist/index.html')
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 const port = process.env.port || 3000
